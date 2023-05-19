08a90d00d8cbddb6d782782d6645dbd70ce36385
Leonardo <leonardo.gobbo@xgen.com.br>
2023-05-19 15:34:13 -0300

    metodo1

--- anotherCardConstructor.js
+++ anotherCardConstructor.js
@@ -1,17 +1,33 @@
-let ouros4 = { Name: '4', manilha: 'ouros', value: 1, pick: true }
-let espadas4 = { Name: '4', manilha: 'espadas', value: 1, pick: true }
-let copas4 = { Name: '4', manilha: 'copas', value: 1, pick: true }
-let paus4 = { Name: '4', manilha: 'paus', value: 1, pick: true }
+const card1P1 = document.getElementById('card1P1');
+const card2P1 = document.getElementById('card2P1');
+const card3P1 = document.getElementById('card3P1');
 
-let ouros5 = { Name: '5', manilha: 'ouros', value: 2, pick: true }
-let espadas5 = { Name: '5', manilha: 'espadas', value: 2, pick: true }
-let copas5 = { Name: '5', manilha: 'copas', value: 2, pick: true }
-let paus5 = { Name: '5', manilha: 'paus', value: 2, pick: true }
+const card1P2 = document.getElementById('card1P2');
+const card2P2 = document.getElementById('card2P2');
+const card3P2 = document.getElementById('card3P2');
 
-let ouros6 = { Name: '5', manilha: 'ouros', value: 3, pick: true }
-let espadas6 = { Name: '5', manilha: 'espadas', value: 3, pick: true }
-let copas6 = { Name: '5', manilha: 'copas', value: 3, pick: true }
-let paus6 = { Name: '5', manilha: 'paus', value: 3, pick: true }
+const card1P3 = document.getElementById('card1P3');
+const card2P3 = document.getElementById('card2P3');
+const card3P3 = document.getElementById('card3P3');
+
+const card1P4 = document.getElementById('card1P4');
+const card2P4 = document.getElementById('card2P4');
+const card3P4 = document.getElementById('card3P4');
+
+let ouros4 = { Name: '4', manilha: 'ouros', value: 1, pick: true };
+let espadas4 = { Name: '4', manilha: 'espadas', value: 1, pick: true };
+let copas4 = { Name: '4', manilha: 'copas', value: 1, pick: true };
+let paus4 = { Name: '4', manilha: 'paus', value: 1, pick: true };
+
+let ouros5 = { Name: '5', manilha: 'ouros', value: 2, pick: true };
+let espadas5 = { Name: '5', manilha: 'espadas', value: 2, pick: true };
+let copas5 = { Name: '5', manilha: 'copas', value: 2, pick: true };
+let paus5 = { Name: '5', manilha: 'paus', value: 2, pick: true };
+
+let ouros6 = { Name: '5', manilha: 'ouros', value: 3, pick: true };
+let espadas6 = { Name: '5', manilha: 'espadas', value: 3, pick: true };
+let copas6 = { Name: '5', manilha: 'copas', value: 3, pick: true };
+let paus6 = { Name: '5', manilha: 'paus', value: 3, pick: true };
 
 let ouros7 = { Name: '7', manilha: 'ouros', value: 4, pick: true };
 let espadas7 = { Name: '7', manilha: 'espadas', value: 4, pick: true };
@@ -58,35 +74,51 @@ let deck = [
     let card;
     function getCard () {
         let randomCard = Math.floor(Math.random() * deck.length);
-        card = deck[randomCard]
+        card = deck[randomCard];
         if(card.pick == false) {
             getCard();
         }
         card.pick = false;
     }
     function player1Deck () {
-        for (let i = 0; i < 3; i++) {
+        //for (let i = 0; i < 3; i++) { TODO: TALVEZ MUDE O METODO PARA QUE A APLICACAO SEJA ESCALAVEL
             getCard();
-            console.log(card);
-        }
+            card1P1.innerText = card.Name + ' ' + card.manilha;
+            getCard();
+            card2P1.innerText = card.Name + ' ' + card.manilha;
+            getCard();
+            card3P1.innerText = card.Name + ' ' + card.manilha;
+        //}
     }
     function player2Deck () {
-        for (let i = 0; i < 3; i++) {
+        //for (let i = 0; i < 3; i++) {
             getCard();
-            console.log(card);
-        }
+            card1P2.innerText = card.Name + ' ' + card.manilha;
+            getCard();
+            card2P2.innerText = card.Name + ' ' + card.manilha;
+            getCard();
+            card3P2innerText = card.Name + ' ' + card.manilha;
+        //}
     }
     function player3Deck () {
-        for (let i = 0; i < 3; i++) {
+        //for (let i = 0; i < 3; i++) {
             getCard();
-            console.log(card);
-        }
+            card1P3.innerText = card.Name + ' ' + card.manilha;
+            getCard();
+            card2P3.innerText = card.Name + ' ' + card.manilha;
+            getCard();
+            card3P3.innerText = card.Name + ' ' + card.manilha;
+        //}
     }
     function player4Deck () {
-        for (let i = 0; i < 3; i++) {
+        //for (let i = 0; i < 3; i++) {
             getCard();
-            console.log(card);
-        }
+            card1P4.innerText = card.Name + ' ' + card.manilha;
+            getCard();
+            card2P4.innerText = card.Name + ' ' + card.manilha;
+            getCard();
+            card3P4.innerText = card.Name + ' ' + card.manilha;
+        //}
     }
     player1Deck();  
     player2Deck();  
